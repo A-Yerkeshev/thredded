0140009b9e362989301685dba880b39f4569e1a2
Arman Yerkesh <a.yerkesh@gmail.com>
2025-07-16 08:59:56 +0300

    Adjust UserPermissions::Admin::#thredded_admin? to accept "forum" argument in multitenant systems

--- app/models/thredded/user_permissions/admin/forum_administrators.rb
+++ app/models/thredded/user_permissions/admin/forum_administrators.rb
@@ -5,8 +5,9 @@ module Thredded
     module Admin
       module ForumAdministrators
         # @return [boolean] Whether this user has full admin rights on Thredded's forum.
-        def thredded_admin?
-          send(Thredded.admin_column)
+        def thredded_admin?(forum)
+          send(Thredded.admin_column) && Thredded::ForumUser.find_by(
+                                          thredded_forum_id: forum.id, thredded_user_detail_id: thredded_user_detail.id)
         end
       end
     end
